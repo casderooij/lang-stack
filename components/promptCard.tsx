18b8784570f4b9652de8f758a8787a74b226a8ce
Cas de Rooij <hello@casderooij.nl>
2025-07-13 22:25:51 +0200

    check input

--- components/promptCard.tsx
+++ components/promptCard.tsx
@@ -1,18 +1,20 @@
 import { motion, Variants } from 'motion/react'
-import { useState } from 'react'
+import { ChangeEvent, FormEvent, useState } from 'react'
 
 interface PromptCardProps {
   index: number
   prompt: string
+  answer: string
   isTopCard: boolean
-  onClick: () => void
+  onSuccess: () => void
 }
 
 export function PromptCard({
   index,
   prompt,
+  answer,
   isTopCard,
-  onClick,
+  onSuccess,
 }: PromptCardProps) {
   const [angle] = useState(
     (index + 1) *
@@ -22,6 +24,7 @@ export function PromptCard({
   )
   const [radius] = useState(() => (isTopCard ? 0 : 100 + 2 * index))
   const [rotation] = useState(Math.sin((index + 1) * 4) * (angle * 0.1))
+  const [input, setInput] = useState('')
 
   const variants: Variants = {
     initial: {
@@ -31,8 +34,8 @@ export function PromptCard({
       rotate: rotation,
     },
     reveal: {
-      x: Math.cos(angle) * radius,
-      y: Math.sin(angle) * radius,
+      x: isTopCard ? 0 : Math.cos(angle) * radius,
+      y: isTopCard ? 0 : Math.sin(angle) * radius,
       opacity: 1,
       rotate: isTopCard ? 0 : rotation,
       transition: {
@@ -42,15 +45,46 @@ export function PromptCard({
     },
   }
 
+  function handleChange(event: ChangeEvent<HTMLInputElement>) {
+    setInput(event.target.value)
+  }
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault()
+
+    if (validateAnswer(input)) {
+      onSuccess()
+    } else {
+      return null
+    }
+  }
+
+  function validateAnswer(input: string): boolean {
+    return input.trim() === answer
+  }
+
   return (
     <motion.div
       className="absolute top-1/2 left-1/2 h-96 w-72 -translate-1/2 rounded-lg bg-stone-50 p-4 text-red-400 drop-shadow-2xl"
       initial="initial"
       animate="reveal"
       variants={variants}
-      onClick={onClick}
+      tabIndex={isTopCard ? 1 : 0}
     >
       <p className="font-mono text-2xl">{prompt}</p>
+
+      {isTopCard && (
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="answer"
+            autoFocus
+            autoComplete="off"
+            onChange={handleChange}
+            className="rounded border border-red-400 p-2 outline-red-400 focus:outline-2"
+          />
+        </form>
+      )}
     </motion.div>
   )
 }
--- components/promptCards.tsx
+++ components/promptCards.tsx
@@ -10,7 +10,12 @@ interface PromptCardsProps {
 }
 
 export function PromptCards({ cards }: PromptCardsProps) {
+  const [cardStack, setCardStack] = useState(cards)
   const [topCardId, setTopCardId] = useState(cards[cards.length - 1].id)
+
+  if (cardStack.length === 0) {
+    return <div></div>
+  }
 
   return (
     <motion.div
@@ -18,13 +23,19 @@ export function PromptCards({ cards }: PromptCardsProps) {
       transition={{ delayChildren: stagger(0.2) }}
       className="absolute inset-0 top-1/2 left-1/2 -translate-1/2 border border-red-300"
     >
-      {cards.map((card, index) => (
+      {cardStack.map((card, index) => (
         <PromptCard
           key={card.id}
           index={index}
           prompt={card.prompt}
+          answer={card.answer}
           isTopCard={topCardId === card.id}
-          onClick={() => setTopCardId(card.id)}
+          onSuccess={() => {
+            if (cardStack.length > 1) {
+              setTopCardId(cardStack[cardStack.length - 2].id)
+            }
+            setCardStack((cards) => cards.filter(({ id }) => id !== card.id))
+          }}
         />
       ))}
     </motion.div>
